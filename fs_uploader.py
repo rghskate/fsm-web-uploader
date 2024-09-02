ba424ae5e6fe136c57df89e3279616f1834dd49b
Rob Hayes <48999578+rghs@users.noreply.github.com>
2024-09-02 08:42:30 +0100

    update fs_uploader

--- fs_uploader.py
+++ fs_uploader.py
@@ -2,10 +2,11 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import pandas as pd
 import os
-from ftplib import FTP, error_perm
+from ftplib import FTP
 from dotenv import dotenv_values
 from time import sleep
 import argparse
+import hashlib
 
 def overprint(content):
     print(end='\x1b[2K')
@@ -25,36 +26,40 @@ def remove_isu(filepath, replace='GBR'):
     with open(filepath, 'w') as f:
         f.write(data)
 
-def get_mtime_remote(ftp:FTP, remote_file:str):
-    response = ftp.sendcmd(f'MDTM {remote_file}')
-    mtime = response[4:]
-    return datetime.strptime(mtime,"%Y%m%d%H%M%S")
-
-def get_mtime_local(local_file:str):
-    return datetime.fromtimestamp(os.path.getmtime(local_file))
-
-def upload_newer(ftp:FTP, filename:str):
-    overprint(f'Checking {filename} on remote...')
+def hash_sha256(filepath:str):
     try:
-        remote_mtime = get_mtime_remote(ftp, filename)
-    except error_perm:
-        remote_mtime = None
-    
-    local_mtime = get_mtime_local(filename)
+        with open(filepath,'rb') as f:
+            file_hash = hashlib.sha256()
+            while chunk := f.read(8192):
+                file_hash.update(chunk)
+        return file_hash.hexdigest()
+    except FileNotFoundError:
+        return None
 
-    update_status = False
+def upload_updated_files(ftp:FTP, current_filetable:pd.DataFrame, old_filetable:pd.DataFrame):
+    old_filetable = old_filetable.reset_index(drop=True)
+    update_counter = 0
+    brand_new_files = current_filetable.loc[~current_filetable['filepaths'].isin(old_filetable['filepaths']),'filepaths']
+    comparable_files = current_filetable.loc[current_filetable['filepaths'].isin(old_filetable['filepaths']),:].reset_index(drop=True)
 
-    if remote_mtime is None or local_mtime > remote_mtime:
-        with open(filename,'rb') as file:
-            ftp.storbinary(f'STOR {filename}', file)
-            overprint(f'Updated {filename} on remote')
-            update_status=True
+    if len(brand_new_files) > 0:
+        for path in brand_new_files:
+            with open(path,'rb') as file:
+                ftp.storbinary(f'STOR {path}', file)
+                overprint(f'Updated {path} on remote')
+            update_counter += 1
+
+    for i in range(0,len(comparable_files)):
+        if comparable_files.loc[i,'hashes'] != old_filetable.loc[i,'hashes']:
+            with open(comparable_files.loc[i,"filepaths"],'rb') as file:
+                ftp.storbinary(f'STOR {comparable_files.loc[i,"filepaths"]}', file)
+                overprint(f'Updated {comparable_files.loc[i,"filepaths"]} on remote')
+            update_counter += 1
     
-    return update_status
+    return update_counter
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('DIR',help='Target directory containing an FS Manager generated site. Program will throw an error if the ISU copyright notice at the bottom of the index page has been removed.')
     parser.add_argument('ENV',help='Environment file containing the following parameters: USER, PASSWORD, HOSTNAME, PORT, REMOTE_DIR')
     parser.add_argument('--sleep-interval','-s',help='Time in seconds to wait between update cycles. Defaults to 5.',default=5)
     parser.add_argument('--time','-t',help='Set a manual time to check segments against. Must be set in "YYYY-MM-DD HH:MM:SS" format.')
@@ -67,7 +72,9 @@ def main():
 
     sleep_interval = int(args.sleep_interval)
     env = dotenv_values(os.path.normpath(os.path.abspath(args.ENV)))
-    local_dir = os.path.normpath(os.path.abspath(args.DIR))
+    replace_isu = env.get('REPLACE_ISU')
+    keepalive_interval = 780
+    local_dir = os.path.normpath(os.path.normpath(os.path.abspath(env['LOCAL_DIR'])))
     os.chdir(local_dir)
 
     try:
@@ -111,8 +118,9 @@ def main():
     segments.loc[:,'segment_judges_link'] = segments.loc[:,'segment_link'].str.replace('.htm','OF.htm')
 
     # Remove references to ISU from judges pages
-    for page in segments.loc[:,'segment_judges_link']:
-        remove_isu(page, replace='GBR')
+    if replace_isu is not None:
+        for page in segments.loc[:,'segment_judges_link']:
+            remove_isu(page, replace='GBR')
 
     print(f'Connecting to FTP site {env["HOSTNAME"]} as {env["USER"]}...')
 
@@ -143,34 +151,47 @@ def main():
         print('\nPlease review your connection settings in your environment file.')
         exit(1)
     
-    print('\nChecking filelist against remote.')
+    print('\nMonitoring local directory for changes...')
     print('\nTo close connection, press Ctrl-C.\n')
+
     
+    filetable_current = pd.DataFrame({'filepaths':'',
+                                          'hashes':''}, index = [0])
     try:
         while True:
+            overprint('Checking for changes...')
+            filetable_previous = filetable_current
             local_filelist = pd.Series(os.listdir('.'))
-            update_counter=0
+
             if manual_time is not None:
                 test_time = manual_time
             else:
                 test_time = datetime.now()
-
             files_to_ignore = segments['segment_judges_link'][segments['date_obj'] > test_time]
 
-            filelist_to_check = local_filelist[~local_filelist.isin(files_to_ignore)]
+            filetable_current = pd.DataFrame({'filepaths':local_filelist[~local_filelist.isin(files_to_ignore)],
+                                            'hashes':local_filelist[~local_filelist.isin(files_to_ignore)].apply(hash_sha256)})
 
-            for file in filelist_to_check:
-                update_status = upload_newer(ftp, file)
-                if update_status is True:
-                    update_counter+=1
-            
-            overprint(f'Updated {update_counter} on last round. Waiting for {sleep_interval} seconds...')
+            update_counter = upload_updated_files(ftp, filetable_current, filetable_previous)
+
+            if update_counter == 0:
+                if (datetime.now()-time_last_update).total_seconds() >= keepalive_interval:
+                    overprint(f'Time since last update exceeds {int(keepalive_interval/60)} minutes. Sending keepalive signal...')
+                    ftp.getwelcome()
+                    time_last_update = datetime.now()
+                else:
+                    overprint(f'Time since last update = {(datetime.now()-time_last_update)}')
+            else:
+                overprint(f'Updated {update_counter} on last round.')
+                time_last_update = datetime.now()
 
             sleep(sleep_interval)
     except KeyboardInterrupt:
         print("\n\nClosing...")
     
     ftp.quit()
+    print(replace_isu)
+    print('Connection closed.')
     exit(0)
 
 if __name__ == "__main__":
